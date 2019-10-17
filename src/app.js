ee1a99d58c191907807bfc0e4651d5a778399f92
Gabriel Martinez <gabriel12martinez21@gmail.com>
2019-10-17 14:44:06 -0500

    Primera parte del juego

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import Paddle from "./paddle.js";
+
+let canvas = document.getElementById('gameScreen');
+let ctx = canvas.getContext('2d');
+
+
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+ 
+let paddle = new Paddle(GAME_WIDTH,GAME_HEIGHT);
+
+paddle.draw(ctx);
+
+let lastTime=0;
+
+function gameLoop(timestamp){
+
+    let deltaTime = timestamp - lastTime;
+    lastTime = timestamp
+
+    ctx.clearRect(0,0,800,600);
+    paddle.update(deltaTime);
+    paddle.draw(ctx);
+
+
+    requestAnimationFrame(gameLoop)
+}
+
+gameLoop()
---
+++ src/paddle.js
@@ -0,0 +1,24 @@
+export default class Paddle{
+    constructor(gameWidth, gameHeight){
+        this.width=150;
+        this.height=30;
+        
+        this.position={
+            x:gameWidth / 2 - this.width / 2,
+            y:gameHeight - this.height - 10
+        };
+    }
+
+    draw(ctx){
+        ctx.fillStyle="#f00"
+        ctx.fillRect(this.position.x,this.position.y,this.width, this.height)
+    }
+    
+    //Bucle
+
+    update(deltaTime){
+        if(!deltaTime) return;
+        this.position.x += 5/deltaTime
+    }
+
+}
